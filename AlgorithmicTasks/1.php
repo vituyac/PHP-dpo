52f5804f3e35753ea3e53ff36c1f68265fbe15f6
cmpy <skisterev78@mail.ru>
2025-02-21 08:28:09 +0300

    refactor: optimization task 1

--- AlgorithmicTasks/1.php
+++ AlgorithmicTasks/1.php
@@ -24,13 +24,8 @@
             }
             else {
                 $temp = 0;
-                if ($bets[$i][2] == 'L') {
-                    $temp = 1;
-                } elseif ($bets[$i][2] == 'R') {
-                    $temp = 2;
-                } elseif ($bets[$i][2] == 'D') {
-                    $temp = 3;
-                }
+                $index = ['L' => 1, 'R' => 2, 'D' => 3];
+                $temp = $index[$games[$bets[$i][0]-1][4]];
                 $balance += (($bets[$i][1] * $games[$bets[$i][0]-1][$temp]) - $bets[$i][1]);
             }
         }
